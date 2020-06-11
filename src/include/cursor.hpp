c46cc012edc133690ec675f89c758fd60aac3ffe
ianmartinez <ianmartinez97@live.com>
2020-06-10 23:55:02 -0500

    Add events TODO

--- src/include/cursor.hpp
+++ src/include/cursor.hpp
@@ -7,6 +7,11 @@
 #include "server.hpp"
 #include "view.hpp"
 
+/**
+ * TODO: Move all event handlers, in the format of:
+ *      void some_func(struct wl_listener *listener, void *data);
+ * to the events and include/events directories.
+ */
 void server_cursor_motion(struct wl_listener *listener, void *data);
 void server_cursor_motion_absolute(struct wl_listener *listener, void *data);
 void server_cursor_button(struct wl_listener *listener, void *data);

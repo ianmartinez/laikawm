a966005800602b515a12f9276303558b45e548b3
ianmartinez <ianmartinez97@live.com>
2020-06-15 13:35:33 -0500

    Fix free() invalid pointer due to typo in wl_container_of()

--- src/events/decoration_events.cpp
+++ src/events/decoration_events.cpp
@@ -3,10 +3,9 @@
 
 void decoration_handle_destroy(struct wl_listener *listener, void *data) {
     auto decoration_mgr = (lk_decoration_manager *)data;
-    lk_server_decoration *decoration = wl_container_of(listener, decoration, mode);
+    lk_server_decoration *decoration = wl_container_of(listener, decoration, destroy);
     decoration_mgr->decorations.remove(decoration);
-    
-    // TODO: Cleanup decoration
+    delete decoration;
 }
 
 void decoration_handle_mode(struct wl_listener *listener, void *data) {
@@ -22,12 +21,10 @@ void decoration_handle_mode(struct wl_listener *listener, void *data) {
 
 void handle_decoration(struct wl_listener *listener, void *data) {
     auto wlr_decoration = (struct wlr_server_decoration *)data;
-    struct lk_decoration_manager *decoration_mgr = wl_container_of(listener, decoration_mgr, server_decoration);
+    lk_decoration_manager *decoration_mgr = wl_container_of(listener, decoration_mgr, server_decoration);
     lk_server_decoration *decoration = new lk_server_decoration();
     decoration->server = decoration_mgr->server;
-
     decoration->wlr_server_decoration = wlr_decoration;
-    decoration->server = decoration_mgr->server;
 
     wl_signal_add(&wlr_decoration->events.destroy, &decoration->destroy);
     decoration->destroy.notify = decoration_handle_destroy;
